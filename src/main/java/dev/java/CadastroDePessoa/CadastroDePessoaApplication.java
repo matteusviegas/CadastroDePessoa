866220d181f81e9817213ecd021b9efab7f70bf7
matteusviegas <msmatteusbr2@gmail.com>
2025-10-01 12:25:45 -0300

    first commit

---
+++ src/main/java/dev/java/CadastroDePessoa/CadastroDePessoaApplication.java
@@ -0,0 +1,13 @@
+package dev.java.CadastroDePessoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDePessoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDePessoaApplication.class, args);
+	}
+
+}
